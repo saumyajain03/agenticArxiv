37690221a535ed2b7f95da41e1ecc4492fefcd40
Saumya Jain <saumyyaaa.03@gmail.com>
2026-07-20 14:20:42 +0530

    perf: reduce workers to 1 and lazy-load docling to fit inside 512MB RAM

--- src/services/pdf_parser/docling.py
+++ src/services/pdf_parser/docling.py
@@ -4,9 +4,7 @@ from pathlib import Path
 from typing import Optional
 
 import pypdfium2 as pdfium
-from docling.datamodel.base_models import InputFormat
-from docling.datamodel.pipeline_options import PdfPipelineOptions
-from docling.document_converter import DocumentConverter, PdfFormatOption
+
 from src.exceptions import PDFParsingException, PDFValidationError
 from src.schemas.pdf_parser.models import PaperFigure, PaperSection, PaperTable, ParserType, PdfContent
 
@@ -33,6 +31,10 @@ class DoclingParser:
         if self.use_lightweight:
             self._converter = None
         else:
+            from docling.datamodel.base_models import InputFormat
+            from docling.datamodel.pipeline_options import PdfPipelineOptions
+            from docling.document_converter import DocumentConverter, PdfFormatOption
+
             # Configure pipeline options
             pipeline_options = PdfPipelineOptions(
                 do_table_structure=do_table_structure,
